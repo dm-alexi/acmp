b5ac8d38c8c8c38ac1162162cefaa71cd4d118b1
dm-alexi <dm2.alexi@gmail.com>
2019-08-22 22:51:01 +0300

    0003: c improved

--- 0001_0100/0003/main.c
+++ 0001_0100/0003/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
- 
+
 int main()
 {
-    long a; 
     FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
+    int a;
 
-    fscanf(f, "%ld", &a); 
+    fscanf(f, "%d", &a);
     a /= 10;
     a = a * (a + 1);
     if (a > 0)
-        fprintf(q, "%ld", a);
+        fprintf(q, "%d", a);
     fprintf(q, "25");
     return 0;
 }
