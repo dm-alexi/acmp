ce853a8996fa9d0a64a48c8abe2e7665803b048d
dm-alexi <dm2.alexi@gmail.com>
2019-08-30 12:47:15 +0300

    0093: python improved

--- 0001_0100/0093/0093.py
+++ 0001_0100/0093/0093.py
@@ -1,11 +1,13 @@
-import re
-
 def comp(s, t):
     n = len(s)
     k = sum(1 for i in range(n) if s[i] != t[i]) if len(t) == n else 0
     return 1 if k == 1 else 0            
 
 with open("input.txt", "r") as f, open("output.txt", "w") as q:
-    s, t = (x.split() for x in re.split("\d+", f.read())[1:])
+    s, t = [], []
+    for i in range(int(f.readline())):
+        s.append(f.readline().strip())
+    for i in range(int(f.readline())):
+        t.append(f.readline().strip())
     q.write(' '.join([str(sum(comp(a, b) for b in t)) for a in s]))
         
