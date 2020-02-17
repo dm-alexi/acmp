dda69a10e171c832c32a72da64e587ac1b1c2204
dm-alexi <telefrag@mail.ru>
2020-02-17 16:50:32 +0300

    retab started

--- 0001_0100/0002/main.c
+++ 0001_0100/0002/main.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
-    int a;
+	FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
+	int a;
 
-    fscanf(f, "%d", &a);
-    fprintf(q, "%d", a > 0 ? (1 + a) * a / 2 : (1 - a) * a / 2 + 1);
-    return 0;
+	fscanf(f, "%d", &a);
+	fprintf(q, "%d", a > 0 ? (1 + a) * a / 2 : (1 - a) * a / 2 + 1);
+	return 0;
 }
--- 0001_0100/0003/main.c
+++ 0001_0100/0003/main.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
-    int a;
+	FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
+	int a;
 
-    fscanf(f, "%d", &a);
-    a /= 10;
-    a = a * (a + 1);
-    if (a > 0)
-        fprintf(q, "%d", a);
-    fprintf(q, "25");
-    return 0;
+	fscanf(f, "%d", &a);
+	a /= 10;
+	a = a * (a + 1);
+	if (a > 0)
+		fprintf(q, "%d", a);
+	fprintf(q, "25");
+	return 0;
 }
