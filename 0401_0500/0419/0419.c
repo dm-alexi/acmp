dd517e161cd0023e826b6c8e32e299a5c0a590bd
dm-alexi <telefrag@mail.ru>
2020-08-18 11:37:38 +0300

    0419: c upd

--- 0401_0500/0419/0419.c
+++ 0401_0500/0419/0419.c
@@ -2,6 +2,20 @@
 #include <ctype.h>
 #include <string.h>
 
+int delAt(char *s, int n, int k)
+{
+	char c = s[k];
+	int r = 1;
+
+	memmove(s + k, s + k + 1, n - k);
+	for (int i = 0; r && i < (n - 1) / 2; ++i)
+		if (s[i] != s[n - 2 - i])
+			r = 0;
+	if (!r)
+		memmove(s + k + 1, s + k, n - k), s[k] = c;
+	return r;
+}
+
 int main()
 {
 	FILE *f = fopen("input.txt", "r"), *q = fopen("output.txt", "w");
@@ -20,18 +34,10 @@ int main()
 				r = 0;
 	if (r && k > -1)
 		s[k] = s[n - k - 1];
-	if (!r && (s[k + 1] == s[n - 1 - k] || s[k] == s[n - 2 - k]))
-	{
-		r = 1;
-		if (s[k + 1] == s[n - 1 - k])
-			memmove(s + k, s + k + 1, n - k);
-		else
-			memmove(s + n - k - 1, s + n - k, k + 1);
-		--n;
-		for (int i = 0; r && i < n / 2; ++i)
-			if (s[i] != s[n - 1 - i])
-				r = 0;
-	}
+	if (!r && s[k + 1] == s[n - 1 - k])
+		r = delAt(s, n, k);
+	if (!r && s[k] == s[n - 2 - k])
+		r = delAt(s, n, n - k - 1);
 	fprintf(q, r ? "YES\n%s" : "NO", s);
 	return 0;
 }
